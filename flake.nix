16920d11344e067c6ec23c9677e6cc4193f52d9b
Jose San Leandro Armendáriz <github@acm-sl.org>
2024-11-17 08:25:27 +0100

    Support for Python 3.12

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
       inputs.nixos.follows = "nixos";
       inputs.pythoneda-shared-pythonlang-banner.follows =
         "pythoneda-shared-pythonlang-banner";
-      url = "github:pythoneda-shared-pythonlang-def/domain/0.0.70";
+      url = "github:pythoneda-shared-pythonlang-def/domain/0.0.71";
     };
   };
   outputs = inputs:
