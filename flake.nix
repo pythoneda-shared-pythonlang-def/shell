c74b58f3616c9d508ccbe0fdd8d3cdd2a61786bc
rydnr <github@acm-sl.org>
2024-12-11 13:28:32 +0100

    Fixed pythonpackage typo in pyproject.py

--- flake.nix
+++ flake.nix
@@ -82,7 +82,7 @@
               authors = builtins.concatStringsSep ","
                 (map (item: ''"${item}"'') maintainers);
               desc = description;
-              inherit homepage pname pythonMajorMinorVersion pythonpackage
+              inherit homepage pname pythonMajorMinorVersion package
                 version;
               pythonedaSharedPythonlangDomain =
                 pythoneda-shared-pythonlang-domain.version;
